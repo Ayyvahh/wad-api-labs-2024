9c2e9f845068f157a8060455630809c9d28653a6
Ava Neary <ayyvah@hotmail.com>
2025-01-06 05:14:50 +0000

    Salt and Hash

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const Schema = mongoose.Schema;
 
@@ -7,14 +8,36 @@ const UserSchema = new Schema({
     password: {type: String, required: true }
 });
 
-const passwordValidator = (password) => {
-    const pwdPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-    return pwdPattern.test(password);
+UserSchema.methods.comparePassword = async function (passw) {
+    return await bcrypt.compare(passw, this.password);
 };
 
-UserSchema.path("password").validate({
-    validator: passwordValidator,
+UserSchema.statics.findByUserName = function (username) {
+    return this.findOne({ username: username });
+};
+
+UserSchema.pre('save', async function(next) {
+    const saltRounds = 10; // You can adjust the number of salt rounds
+    //const user = this;
+    if (this.isModified('password') || this.isNew) {
+        try {
+            const hash = await bcrypt.hash(this.password, saltRounds);
+            this.password = hash;
+            next();
+        } catch (error) {
+            next(error);
+        }
+
+    } else {
+        next();
+    }
 });
 
+const passwordValidator = (password) => {
+    const regexString = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+    return regexString.test(password);
+};
+
+UserSchema.path("password").validate(passwordValidator);
 
 export default mongoose.model('User', UserSchema);
